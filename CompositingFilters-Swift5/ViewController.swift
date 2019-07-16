2b8017049553d2bc185bb27ee1696617ef48023d
Austin Kwong <augustine@nex.team>
2019-07-16 09:15:25 -0700

    Initial Commit

---
+++ CompositingFilters-Swift5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompositingFilters-Swift5
+//
+//  Created by Austin Kwong on 7/16/19.
+//  Copyright © 2019 Austin Kwong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CompositingFilters-Swift5Tests/CompositingFilters_Swift5Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CompositingFilters_Swift5Tests.swift
+//  CompositingFilters-Swift5Tests
+//
+//  Created by Austin Kwong on 7/16/19.
+//  Copyright © 2019 Austin Kwong. All rights reserved.
+//
+
+import XCTest
+@testable import CompositingFilters_Swift5
+
+class CompositingFilters_Swift5Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
